52eabf08b64b4b44f55397b29a693173f5596c2f
ZoVoS <aaron.trevallion@zovos.com>
2023-10-22 20:18:56 +0100

    Update server.py
    Signed-off-by: ZoVoS <aaron.trevallion@zovos.com>

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect
+from flask import Flask, send_from_directory, render_template, redirect, jsonify
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
 app = Flask(__name__)
-
 port = int(os.environ.get("PORT", 5000))
 
 @app.route('/static/<path:path>')
@@ -11,7 +13,22 @@ def serve_static(path):
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/scrape')
+def scrape():
+    # Your scraping logic here
+    url = "https://www.bmh-ltd.com/vehicle-parts/mini-parts/underframe/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', {'id': 'tablepress-2'})
+    rows = table.find_all('tr')
+    header = [th.text for th in rows[0].find_all('th')]
+    data = [[td.text for td in row.find_all('td')] for row in rows[1:]]
+    df = pd.DataFrame(data, columns=header)
+    json_output = df.to_json(orient='split')
+    
+    return jsonify(json_output)
 
 @app.route('/<path:path>')
 def all_routes(path):
